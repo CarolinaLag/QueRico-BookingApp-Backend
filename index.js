29bce0cec8e0c169587358df542faf06b6061544
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-08-24 13:04:51 +0200

    fixed path env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "./.env" });
 app.use(cors());
 app.use(express.json());
 
